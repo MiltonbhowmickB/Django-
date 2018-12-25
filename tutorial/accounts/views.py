418953747001ed9cfa92a05d00c361aa1896993d
Milton Chandra Bhowmick <miltonbhowmick6@gmail.com>
2018-12-25 12:20:12 +0600

    Add files via upload

--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse("adadnand")
+	return render(request, 'accounts/attendance_html.html')
