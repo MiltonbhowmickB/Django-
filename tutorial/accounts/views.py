74a5a8fa9b8c0776030eddd1b8b8b931993c37ad
Milton Chandra Bhowmick <miltonbhowmick6@gmail.com>
2018-12-25 22:54:45 +0600

    Add files via upload

--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-	return render(request, 'accounts/attendance_html.html')
+	link_name = ["Link1","Link2","Link3"]
+	online_link = ["https://www.google.com","https://www.facebook.com","https://www.w3schools.com"]
+	name = "contact info"
+	args = {'title':name, 'names':link_name, 'links':online_link}
+	return render(request, 'accounts/attendance_html.html', args) 
