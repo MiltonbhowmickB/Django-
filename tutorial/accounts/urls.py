7f28874151d5a9e8e5cda23226377891a3202a8e
Milton Chandro Bhowmick <miltonbhowmick6@gmail.com>
2018-12-27 12:10:22 +0600

    Add files via upload

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
-
+from django.contrib.auth import views as auth_views
+from django.urls import path
 urlpatterns = [
-	url(r'^$', views.home),
+	path('', views.home),
+	path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html')),
+	path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html')),
+	path('register/', views.register, name='register')	
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from accounts.forms import RegistrationForm
 
 # Create your views here.
 
@@ -6,5 +7,17 @@ def home(request):
 	link_name = ["Link1","Link2","Link3"]
 	online_link = ["https://www.google.com","https://www.facebook.com","https://www.w3schools.com"]
 	name = "contact info"
-	args = {'title':name, 'names':link_name, 'links':online_link}
-	return render(request, 'accounts/attendance_html.html', args) 
+	zips = zip(link_name, online_link)
+	args = {'title':name, 'names':link_name, 'links':online_link, 'zipped':zips}
+	return render(request, 'accounts/attendance_html.html', args)
+
+def register(request): 
+	if request.method == ['POST']:
+		form = RegistrationForm[request.POST]
+		if form.is_valid():
+			form.save()
+			return redirect('/accounts')
+	else:
+		form = RegistrationForm()
+		args = {'form': form}
+		return render(request, 'accounts/reg_form.html', args)
